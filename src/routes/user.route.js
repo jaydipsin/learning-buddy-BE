92e95b32437b55b8c16d2397405edc3dad07ae64
jaydip singh <jaydipsinghchouhan93@gmail.com>
2026-03-31 20:03:54 +0530

    Fixes

--- src/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { handleUserProfile } from "../controllers/user.controller.js";
-
-const router = express.Router();
-
-router.get("/profile", authMiddleware, handleUserProfile);
-
-export default router;
